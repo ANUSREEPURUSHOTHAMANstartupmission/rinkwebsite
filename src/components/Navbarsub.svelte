9d1622c92983464695101f3a376b57aaa3ed8c01
Anusree Purushothaman <anusreepurushothaman@MacBook-Pro.local>
2025-08-11 12:31:09 +0530

    changes

--- src/components/Navbarsub.svelte
+++ src/components/Navbarsub.svelte
@@ -56,8 +56,8 @@
   
   <svelte:window bind:scrollY={y}/>
   
-    <div id="navbar"  class="fixed space-x-4  md:space-x-0 min-h-fit w-screen sm:min-h-0  left-0
-   flex sm:items-center
+    <div id="navbar"  class="fixed space-x-4  lg:space-x-0 min-h-fit w-screen lg:min-h-0  left-0
+   flex lg:items-center
     ease-in-out transition-all transform duration-300
     z-50  py-2
            {scrolled ? 'bg-white top-0 shadow-md' : 'bg-white text-black '}
@@ -65,7 +65,7 @@
 
    ">
     <div class="w-full container mx-auto  flex justify-between px-4">
-     <div class=" md:px-2 m-0 font-medium gap-2 w-fit p-0 rounded-md flex items-center justify-center ">
+     <div class=" lg:px-2 m-0 font-medium gap-2 w-fit p-0 rounded-md flex items-center justify-center ">
      
         <a href="/" >
           <img
@@ -78,18 +78,18 @@
   
      <main class=" flex  items-start px-7 my-auto">
         <div style="font-family: Museo-Sans, sans-serif;" class="flex my-auto  {scrolled?'':''}">
-             <a href="/"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm hover:text-green-900">Home</a>
-            <!-- <a href="/#about"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm hover:text-green-900">About</a> -->
-            <a href="/#What_we_do"  class="page-scroll md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm hover:text-green-900">What we do</a>
+             <a href="/"  class="lg:flex cursor-pointer hidden px-4 py-3  lg:py-1 uppercase   text-sm hover:text-green-900">Home</a>
+            <!-- <a href="/#about"  class="lg:flex cursor-pointer hidden px-4 py-3  lg:py-1 uppercase   text-sm hover:text-green-900">About</a> -->
+            <a href="/#What_we_do"  class="page-scroll lg:flex cursor-pointer hidden px-4 py-3  lg:py-1 uppercase   text-sm hover:text-green-900">What we do</a>
             
-            <a href="https://rinkevents.startupmission.in/" target="_blank" class="md:flex hidden cursor-pointer  px-4 py-3  md:py-1 uppercase   text-sm hover:text-green-900">Events</a>
-            <a href="#contact"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm hover:text-green-900">contact us</a>
-            <a href="/Gallery"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">Gallery</a>
+            <a href="https://rinkevents.startupmission.in/" target="_blank" class="lg:flex hidden cursor-pointer  px-4 py-3  lg:py-1 uppercase   text-sm hover:text-green-900">Events</a>
+            <a href="#contact"  class="lg:flex cursor-pointer hidden px-4 py-3  lg:py-1 uppercase   text-sm hover:text-green-900">contact us</a>
+            <a href="/Gallery"  class="lg:flex cursor-pointer hidden px-4 py-3  lg:py-1 uppercase   text-sm">Gallery</a>
            
         </div>
       
-        <div class="flex  items-center justify-center  bg-black my-auto spl_cursor md:hidden flex ">   
-             <div on:click="{()=> menu_open = !menu_open }" class="z-50 items-center justify-center my-auto spl_cursor md:pt-4 pt-1  bg-black absolute cursor-pointer top-3 md:right-8 right-4 w-10 h-10 md:text-xl text-xs ml-auto transition duration-150  py-1  -solid -transparent rounded bg-transparent block outline-none focus:outline-none" type="button">
+        <div class="flex  items-center justify-center  bg-black my-auto spl_cursor lg:hidden flex ">   
+             <div on:click="{()=> menu_open = !menu_open }" class="z-50 items-center justify-center my-auto spl_cursor lg:pt-4 pt-1  bg-black absolute cursor-pointer top-3 lg:right-8 right-4 w-10 h-10 lg:text-xl text-xs ml-auto transition duration-150  py-1  -solid -transparent rounded bg-transparent block outline-none focus:outline-none" type="button">
                 <div class="block absolute w-6 h-0.5 rounded-sm bg-black top-5  duration-500 { menu_open ? 'opacity-0':'' }" ></div>
                 <div class="block absolute w-6 h-0.5 rounded-sm bg-black top-6 duration-500 { menu_open ? 'transform rotate-45':'' }"></div>
                 <div class="block absolute w-6 h-0.5 rounded-sm bg-black top-7 duration-500 { menu_open ? 'transform -rotate-45':'' }"></div>
@@ -101,7 +101,7 @@
    </div>
   
   
-   <div id="navbar" class=" min-h-screen   h-20 p-8 sm:py-3 bg-white left-0 top-0
+   <div id="navbar" class=" min-h-screen   h-20 p-8 lg:py-3 bg-white left-0 top-0
    flex items-center
     ease-in-out transition-all transform duration-300
    -translate-x-full  fixed z-50
@@ -117,27 +117,27 @@
     </div>
   
   
-    <div class="w-fit container mx-auto sm:items-center sm:justify-between">
-        <div class="mb-8 gap-2  font-medium sm:bg-transparent p-5 sm:p-0 rounded-md flex items-center justify-center">
-            <img src="/img/logo.svg" alt="logo" class="md:h-14 h-12">
+    <div class="w-fit container mx-auto lg:items-center lg:justify-between">
+        <div class="mb-8 gap-2  font-medium lg:bg-transparent p-5 lg:p-0 rounded-md flex items-center justify-center">
+            <img src="/img/logo.svg" alt="logo" class="lg:h-14 h-12">
   
         </div>
          <nav>
-            <ul class=" md:text-sm gap-2 font-data">
-                    <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
+            <ul class=" lg:text-sm gap-2 font-data">
+                    <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 lg:hidden flex">
                         <a href="/"  class="cursor-pointer  font-bold hover: uppercase ">Home</a>
                     </li>
                    
-                    <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
+                    <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 lg:hidden flex">
                       <a href="/#What_we_do"  class="page-scroll cursor-pointer  font-bold hover: uppercase ">What we do</a>
                     </li> 
-                     <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
+                     <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 lg:hidden flex">
                       <a href="https://rinkevents.startupmission.in" target="_blank"  class="cursor-pointer  font-bold hover: uppercase ">Events</a>
                     </li>
-                     <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
-                      <a href="/#contact"  class="cursor-pointer  uppercase ">Contact US</a>
+                     <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 lg:hidden flex">
+                      <a href="/#contact"  class="cursor-pointer  font-bold hover: uppercase ">Contact US</a>
                     </li>
-                      <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
+                      <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 lg:hidden flex">
                       <a href="/Gallery"  class="cursor-pointer  font-bold hover: uppercase ">Gallery</a>
                     </li>
                     
